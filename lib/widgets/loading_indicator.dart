40a155e8d35f1c48acc1737d09792d64f17c4e8b
Jappe02 <69361903+Jappe02@users.noreply.github.com>
2023-05-28 13:23:25 +0300

    Loading indicator

---
+++ lib/widgets/loading_indicator.dart
@@ -0,0 +1,52 @@
+//  JappeOS-DesktopUI, UI Widgets for JappeOS Desktop & Login.
+//  Copyright (C) 2023  Jappe02
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:provider/provider.dart';
+import 'package:shade_theming/shade_theming.dart';
+
+/// A simple loading indicator.
+class DeuiLoadingIndicator extends StatefulWidget {
+  const DeuiLoadingIndicator({Key? key, this.bigger = false}) : super(key: key);
+
+  final bool bigger;
+
+  @override
+  _DeuiLoadingIndicatorState createState() => _DeuiLoadingIndicatorState();
+}
+
+/// The [State] class for [DeuiLoadingIndicator].
+class _DeuiLoadingIndicatorState extends State<DeuiLoadingIndicator> {
+  @override
+  Widget build(BuildContext context) {
+    if (context.watch<ShadeThemeProvider>().getTheme() == 0) {
+      return SvgPicture.asset(
+        'resources/images/loading_lightth.svg',
+        width: !widget.bigger ? 30.0 : 90.0,
+        height: !widget.bigger ? 30.0 : 90.0,
+      );
+    } else {
+      return SvgPicture.asset(
+        'resources/images/loading_darkth.svg',
+        width: !widget.bigger ? 30.0 : 90.0,
+        height: !widget.bigger ? 30.0 : 90.0,
+      );
+    }
+  }
+}
